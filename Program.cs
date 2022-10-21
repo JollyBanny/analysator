7474631940d76b8de4ae91dd1b5e3734d2bdf305
JollyBanny <rootki7@protonmail.com>
2022-10-22 05:07:43 +1000

    Init repo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LexicalAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fstream = new StreamReader("./tests/01.in");
+        }
+    }
+}
+
