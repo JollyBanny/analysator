3d0f437ebe45fce1cebece636dd13dea1aeca42d
JollyBanny <rootki7@protonmail.com>
2022-10-31 21:06:22 +1000

    add char extensions

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,6 +1,6 @@
 using System.Globalization;
 
-namespace LexicalAnalyzer
+namespace LexicalAnalyzer.Extensions
 {
     public static class DoubleExtension
     {
@@ -12,5 +12,27 @@ namespace LexicalAnalyzer
     {
         public static string Capitalize(this string str) =>
             CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+    }
+
+    public static class CharExtension
+    {
+        public static bool IsSpace(this char ch) =>
+            ch == ' ' || ch == '\r' || ch == '\n' || ch == '\t';
+
+        public static bool IsLetter(this char ch) =>
+            'a' <= char.ToLower(ch) && char.ToLower(ch) <= 'z' || ch == '_';
+
+        public static bool IsDigit(this char ch) => '0' <= ch && ch <= '9';
+
+        public static bool IsHex(this char ch) =>
+            IsDigit(ch) || 'a' <= char.ToLower(ch) && char.ToLower(ch) <= 'f';
+
+        public static int DigitValue(this char digit) => digit switch
+        {
+            char ch when '0' <= ch && ch <= '9' => (int)(ch - '0'),
+            char ch when 'a' <= char.ToLower(ch) && char.ToLower(ch) <= 'f' =>
+                (int)(char.ToLower(ch) - 'a' + 10),
+            _ => -1
+        };
     }
 }
